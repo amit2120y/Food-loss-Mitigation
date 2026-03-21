dbb95fa8951e494a484b15d5ea4752eda51b7ceb
narottampandey <narottampandey7781@gmail.com>
2026-03-21 13:03:26 +0530

    annasetu cliet

--- LLMs/APIs_setup/gemini_01_1.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-client = genai.Client(
-    api_key="AIzaSyApTjwCdtjhKzKzwwUO3hr9UEkXoyU4YSs"
-)
-
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents=input("How can i help you?")
-)
-print(response.text)
